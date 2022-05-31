f5b8fb80cfbd6e8d138a74635de83763a2c105fd
indraneel10081998 <indraneelbiswasofficial@gmail.com>
2022-05-31 09:51:47 +0530

    add

---
+++ components/header.php
@@ -0,0 +1,12 @@
+<div class="navbar">
+    <div class="navbar-inner">
+        <a href="index.php" class="brand"><img src="../img/logo.png" alt=""></a>
+        <ul class="nav">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        </ul>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -3,18 +3,7 @@
 <?php include("./components/head.php"); ?>
 
 <body>
-    <div class="navbar">
-        <div class="navbar-inner">
-            <a href="index.php" class="brand">E-Recruit</a>
-            <ul class="nav">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <li><a href="login.php">Login</a></li>
-                <li><a href="register.php">Register</a></li>
-            </ul>
-        </div>
-    </div>
+    <?php include("./components/header.php"); ?>
 </body>
 
 </html>
