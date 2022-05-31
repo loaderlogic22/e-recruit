1c3322b655020c39497fe3caa21e1f3614876e00
indraneel10081998 <indraneelbiswasofficial@gmail.com>
2022-05-31 10:15:26 +0530

    add

--- components/header.php
+++ components/header.php
@@ -1,7 +1,8 @@
 <div class="navbar">
     <div class="navbar-inner">
-        <a href="index.php" class="brand"><img src="../img/logo.png" alt=""></a>
+        <a href="index.php" class="brand"><img src="img/headerlogo.png" alt=""></a>
         <ul class="nav">
+            <div class="closebtn"><img src="icon/close.png" alt=""></div>
             <li><a href="index.php">Home</a></li>
             <li><a href="about.php">About</a></li>
             <li><a href="contact.php">Contact</a></li>
