a07fdd81129c1c0abeadd8be70fdd8c0eebda83c
indraneel10081998 <indraneelbiswasofficial@gmail.com>
2022-05-31 11:33:01 +0530

    add

--- login.php
+++ login.php
@@ -3,9 +3,26 @@
 <?php include("./components/head.php"); ?>
 
 <body>
-    <label for="roll">Roll No</label>
-    <input type="number" name="roll" id="roll">
-    <label for="password">Password</label>
-    <input type="text" name="Password" id="password">
+    <div class="flex">
+    <div class="side-login"></div>
+    <div class="form-login">
+        <div class="login">
+            <form>
+                <h2>Login</h2>
+                <br>
+                <label for="roll">Roll No</label>
+                <br>
+                <input type="number" name="roll" id="roll">
+                <br>
+                <label for="password">Password</label>
+                <br>
+                <input type="text" name="Password" id="password">
+                <br>
+                <input type="button" value="Submit">
+            </form>
+        </div>
+    </div>
+    </div>
 </body>
+
 </html>
